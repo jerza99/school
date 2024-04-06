96e401fcbaca6b20fec000a0b86d1bab5e3d8542
Jerzayl <62535706+jerza99@users.noreply.github.com>
2024-04-06 11:28:34 -0600

    actualizacion css

--- views/login.php
+++ views/login.php
@@ -8,6 +8,7 @@
 
     <!-- links -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css">
    </head>
    <body>
     <div class="form-container" >
